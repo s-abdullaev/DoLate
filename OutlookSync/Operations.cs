d6b40cdc6b1251f18a3bfaca1fcfc1ec4885de8c
unknown <snasser@SNASSERPC.pc.factset.com>
2013-11-30 21:15:55 +0000

    new

---
+++ OutlookSync/Operations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Outlook;
+
+namespace Tasks
+{
+    class Operations
+    {
+        public static TimeSpan CalculateOverdue(_TaskItem task)
+        {
+            TimeSpan overdue = DateTime.Now.Subtract(task.DueDate);
+            return overdue;
+        }
+
+        public static double CalculateTotalFines(double rate, TimeSpan overdue)
+        {
+            double totalAmount = rate * overdue.Hours;
+            return totalAmount;
+        }
+
+        public static double RatePerHour
+        {
+            get
+            {
+                if (ConfigurationManager.AppSettings["RatePerHour"] != null)
+                {
+                    string strPerHour = ConfigurationManager.AppSettings["RatePerHour"];
+                    return Double.Parse(strPerHour);
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            set
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings.Add("RatePerHour", value.ToString());
+                config.Save(ConfigurationSaveMode.Modified);
+            }
+        }
+    }
+}
---
+++ OutlookSync/OutlookSync.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace Tasks
+{
+    class OutlookSync
+    {
+        private Outlook._Application outlookObj = new Outlook.Application();
+
+        public IList<Outlook._TaskItem> AllTasks
+        {
+            get
+            {
+                Outlook.MAPIFolder outlookTasks = (Outlook.MAPIFolder)outlookObj.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderTasks);
+                int numberOfTasks = outlookTasks.Items.Count;
+                IList<Outlook._TaskItem> allTasks = new List<Outlook._TaskItem>(numberOfTasks);
+                foreach (Microsoft.Office.Interop.Outlook.TaskItem taskItem in outlookTasks.Items)
+                {
+                    allTasks.Add(taskItem);
+                }
+
+                return allTasks;
+            }
+        }
+
+
+        public void AddTask(Outlook._TaskItem task)
+        {
+            Outlook.MAPIFolder outlookTasks = (Outlook.MAPIFolder)outlookObj.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderTasks);
+            outlookTasks.Items.Add(task);
+        }
+
+        public void DeleteTask(Outlook._TaskItem task)
+        {
+            task.Delete();
+        }
+
+
+
+
+        static int Main(string[] args)
+        {
+            Operations.RatePerHour = 5;
+            double setting = Operations.RatePerHour;
+            OutlookSync n1 = new OutlookSync();
+            IList<Outlook._TaskItem> allTasks = n1.AllTasks;
+            Console.WriteLine(allTasks[0].Status);
+            Operations.CalculateOverdue(allTasks[0]);
+            Console.Write(allTasks.Count);
+            Console.Read();
+
+            //OutlookSync myOutlook = new OutlookSync();
+            //myOutlook.c_tasks();
+            //myOutlook.iGetAllTaskItems();
+
+
+
+
+
+
+            return 0;
+        }
+    }
+}
